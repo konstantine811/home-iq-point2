33d0995d72151f7f394cff1adb1a148f61e95127
konstantine811 <galystyan777@gmail.com>
2024-09-23 12:09:33 +0300

    Refactor SVG icons in ProjectSteps component and SecondSection component

--- src/components/pages/home/second-section.tsx
+++ src/components/pages/home/second-section.tsx
@@ -1,61 +1,20 @@
 "use client";
 
-import Bathroom from "@/components/svg-icons/bathroom";
-
-const SVG_PATH_CONFIG = [
-  {
-    icon: <Bathroom />,
-    title: "Bathroom Remodel",
-  },
-  // {
-  //   path: () => import("@/assets/svg/kitchen.svg"),
-  //   title: "Kitchen Remodel",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/gutter.svg"),
-  //   title: "Gutter",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/home-security.svg"),
-  //   title: "Home Security",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/hvac.svg"),
-  //   title: "HVAC",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/roofing.svg"),
-  //   title: "Roofing",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/windows.svg"),
-  //   title: "Windows",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/bath-to-shower.svg"),
-  //   title: "Bath to Shower",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/walk-in-tubs.svg"),
-  //   title: "Walk-in Tubs",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/walk-in-shower.svg"),
-  //   title: "Walk-in Shower",
-  // },
-];
+import { SVG_PATH_CONFIG } from "@/configs/cards";
+import dynamic from "next/dynamic";
 
 const SecondSection = () => {
   return (
     <section className="container grid md:grid-cols-5 grid-cols-2 md:gap-8 mb-8">
       {SVG_PATH_CONFIG.map((data, index) => {
+        const SvgComponent = dynamic(data.path);
         return (
           <div
             className="flex flex-col md:p-4 items-center justify-center h-[128px] hover:bg-red-100 transition-all duration-300 rounded-lg cursor-pointer"
             key={index}
           >
-            <div className="grow flex items-end mb-4 w-14 h-14">
-              {data.icon}
+            <div className="mb-4 w-14 h-14">
+              <SvgComponent />
             </div>
             <p className="text-blue-900 font-semibold text-sm text-center pb-2">
               {data.title}
--- src/configs/cards.ts
+++ src/configs/cards.ts
@@ -1,47 +1,46 @@
 import { IReviewCard } from "@/models/data-config/card.model";
 import { ThemeColors } from "./theme-colors";
-
 export const SVG_PATH_CONFIG = [
   {
-    path: () => import("@/components/svg-icons/bathroom"),
+    path: () => import("@/assets/svg/bathroom.svg"),
     title: "Bathroom Remodel",
   },
-  // {
-  //   path: () => import("@/assets/svg/kitchen.svg"),
-  //   title: "Kitchen Remodel",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/gutter.svg"),
-  //   title: "Gutter",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/home-security.svg"),
-  //   title: "Home Security",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/hvac.svg"),
-  //   title: "HVAC",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/roofing.svg"),
-  //   title: "Roofing",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/windows.svg"),
-  //   title: "Windows",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/bath-to-shower.svg"),
-  //   title: "Bath to Shower",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/walk-in-tubs.svg"),
-  //   title: "Walk-in Tubs",
-  // },
-  // {
-  //   path: () => import("@/assets/svg/walk-in-shower.svg"),
-  //   title: "Walk-in Shower",
-  // },
+  {
+    path: () => import("@/assets/svg/kitchen.svg"),
+    title: "Kitchen Remodel",
+  },
+  {
+    path: () => import("@/assets/svg/gutter.svg"),
+    title: "Gutter",
+  },
+  {
+    path: () => import("@/assets/svg/home-security.svg"),
+    title: "Home Security",
+  },
+  {
+    path: () => import("@/assets/svg/hvac.svg"),
+    title: "HVAC",
+  },
+  {
+    path: () => import("@/assets/svg/roofing.svg"),
+    title: "Roofing",
+  },
+  {
+    path: () => import("@/assets/svg/windows.svg"),
+    title: "Windows",
+  },
+  {
+    path: () => import("@/assets/svg/bath-to-shower.svg"),
+    title: "Bath to Shower",
+  },
+  {
+    path: () => import("@/assets/svg/walk-in-tubs.svg"),
+    title: "Walk-in Tubs",
+  },
+  {
+    path: () => import("@/assets/svg/walk-in-shower.svg"),
+    title: "Walk-in Shower",
+  },
 ];
 
 export const ReviewCardItems: IReviewCard[] = [
