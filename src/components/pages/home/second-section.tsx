760415523e6c989490e76ba4ef7e2aaadc0e40d7
konstantine811 <galystyan777@gmail.com>
2024-09-23 11:45:50 +0300

    Refactor import statement in second-section.tsx

--- src/components/pages/home/second-section.tsx
+++ src/components/pages/home/second-section.tsx
@@ -1,7 +1,49 @@
 "use client";
 
-import { SVG_PATH_CONFIG } from "@/components/common/project-steps";
-import dynamic from "next/dynamic";
+import Bathroom from "@/components/svg-icons/bathroom";
+
+const SVG_PATH_CONFIG = [
+  {
+    icon: <Bathroom />,
+    title: "Bathroom Remodel",
+  },
+  // {
+  //   path: () => import("@/assets/svg/kitchen.svg"),
+  //   title: "Kitchen Remodel",
+  // },
+  // {
+  //   path: () => import("@/assets/svg/gutter.svg"),
+  //   title: "Gutter",
+  // },
+  // {
+  //   path: () => import("@/assets/svg/home-security.svg"),
+  //   title: "Home Security",
+  // },
+  // {
+  //   path: () => import("@/assets/svg/hvac.svg"),
+  //   title: "HVAC",
+  // },
+  // {
+  //   path: () => import("@/assets/svg/roofing.svg"),
+  //   title: "Roofing",
+  // },
+  // {
+  //   path: () => import("@/assets/svg/windows.svg"),
+  //   title: "Windows",
+  // },
+  // {
+  //   path: () => import("@/assets/svg/bath-to-shower.svg"),
+  //   title: "Bath to Shower",
+  // },
+  // {
+  //   path: () => import("@/assets/svg/walk-in-tubs.svg"),
+  //   title: "Walk-in Tubs",
+  // },
+  // {
+  //   path: () => import("@/assets/svg/walk-in-shower.svg"),
+  //   title: "Walk-in Shower",
+  // },
+];
 
 const SecondSection = () => {
   return (
