159375d420d70eb62e8495d95e8fa92296fb580c
konstantine811 <galystyan777@gmail.com>
2024-09-23 11:42:40 +0300

    Refactor import statement in second-section.tsx

--- src/components/pages/home/second-section.tsx
+++ src/components/pages/home/second-section.tsx
@@ -1,20 +1,19 @@
 "use client";
 
-import { SVG_PATH_CONFIG } from "@/configs/cards";
+import { SVG_PATH_CONFIG } from "@/components/common/project-steps";
 import dynamic from "next/dynamic";
 
 const SecondSection = () => {
   return (
     <section className="container grid md:grid-cols-5 grid-cols-2 md:gap-8 mb-8">
       {SVG_PATH_CONFIG.map((data, index) => {
-        const SvgComponent = dynamic(data.path);
         return (
           <div
             className="flex flex-col md:p-4 items-center justify-center h-[128px] hover:bg-red-100 transition-all duration-300 rounded-lg cursor-pointer"
             key={index}
           >
             <div className="grow flex items-end mb-4 w-14 h-14">
-              <SvgComponent />
+              {data.icon}
             </div>
             <p className="text-blue-900 font-semibold text-sm text-center pb-2">
               {data.title}
