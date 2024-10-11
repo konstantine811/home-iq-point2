5a47775cfe4c688b5b4b5fec4fbc351ac5ca3665
konstantine811 <galystyan777@gmail.com>
2024-10-11 15:29:33 +0300

    Refactor SecondSection component styles and dimensions

--- src/components/pages/home/second-section.tsx
+++ src/components/pages/home/second-section.tsx
@@ -5,12 +5,12 @@ import dynamic from "next/dynamic";
 
 const SecondSection = () => {
   return (
-    <section className="container grid md:grid-cols-5 grid-cols-2 md:gap-8 mb-8">
+    <section className="container overflow-x-auto md:overflow-visible grid md:grid-cols-5 md:grid-rows-2 grid-flow-col auto-cols-max md:gap-4 mb-8">
       {SVG_PATH_CONFIG.map((data, index) => {
         const SvgComponent = dynamic(data.path);
         return (
           <div
-            className="flex flex-col md:p-4 items-center justify-center h-[128px] hover:bg-red-100 transition-all duration-300 rounded-lg cursor-pointer"
+            className="flex flex-col p-4 items-center justify-center h-[128px] w-[150px] md:w-auto hover:bg-red-100 transition-all duration-300 rounded-lg cursor-pointer"
             key={index}
           >
             <div className="mb-4 w-14 h-14">
