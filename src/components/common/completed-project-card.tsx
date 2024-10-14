fafa94fbad1c365d6736b0159e9ec3a4220a2a64
konstantine811 <galystyan777@gmail.com>
2024-10-14 18:18:05 +0300

    Refactor package dependencies and add react-intersection-observer
    Update package-lock.json and package.json to include the new dependency react-intersection-observer. This will be used in the CompletedProjectCard component to handle intersection observer functionality for animations.
    
    Refactor CompletedProjectCard component and use react-intersection-observer
    
    In the CompletedProjectCard component, remove the framer-motion useInView hook and replace it with the react-intersection-observer useInView hook. This change allows for more control over the animation trigger and improves performance.
    
    Update spring animation settings in CompletedProjectCard component
    
    Adjust the spring animation settings in the CompletedProjectCard component to improve the smoothness and stability of the animation. This includes reducing stiffness and increasing damping.
    
    Add animation reset and start value in CompletedProjectCard component
    
    Before starting the animation in the CompletedProjectCard component, reset the value to 0 to ensure a smooth transition. Then, animate the value to the desired end value.
    
    Refactor InfiniteCarousel component and add InfiniteBannerCarousel component
    
    Make changes to the InfiniteCarousel component and introduce a new component called InfiniteBannerCarousel. This refactor improves the functionality and performance of the carousel feature.

--- src/components/common/completed-project-card.tsx
+++ src/components/common/completed-project-card.tsx
@@ -3,8 +3,9 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { ICardPriceIcon } from "@/models/data-config/card.model";
 import { countDecimalPlaces } from "@/utils/numbers";
-import { useInView, useSpring, motion } from "framer-motion";
+import { useSpring, motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 interface Props {
   data: ICardPriceIcon;
@@ -16,22 +17,30 @@ const CompletedProjectCard = ({
   index,
 }: Props) => {
   const [value, setValue] = useState<number | string>(0);
-  const ref = useRef<HTMLDivElement | null>(null);
-  const isInView = useInView(ref, { margin: "200px", once: true });
+  // Використовуємо хук useInView з react-intersection-observer
+  const { ref, inView, entry } = useInView({
+    threshold: 0.2, // спрацьовує, коли 50% елемента у видимій зоні
+    triggerOnce: true, // анімація спрацьовує лише один раз
+  });
   const spring = useSpring(0, {
-    bounce: 0, // амплітуда
-    duration: 2000, // тривалість в секундах
+    stiffness: 30, // Зменшуємо жорсткість
+    damping: 15, // Збільшуємо демпфування для стабілізації
+    duration: 1, // Зберігаємо тривалість
   });
 
   spring.on("change", (value) => {
     const formattedValue = value.toFixed(countDecimalPlaces(title));
     setValue(formattedValue);
   });
+  // Скидаємо на 0 перед початком анімації
   useEffect(() => {
-    if (isInView) {
+    if (inView && entry) {
+      // Перш ніж почати анімацію, встановлюємо початкове значення на 0
+      spring.set(0);
+      // Після цього запускаємо анімацію до кінцевого значення
       spring.set(title);
     }
-  }, [isInView, spring, title]);
+  }, [inView, entry, spring, title]);
   // Налаштування для плавного зʼявлення картки
   const cardVariants = {
     hidden: { opacity: 0, y: 10 }, // стартова позиція (невидима)
@@ -39,7 +48,7 @@ const CompletedProjectCard = ({
       opacity: 1,
       y: 0,
       transition: {
-        duration: 0.5,
+        duration: 0.3,
         delay: index * 0.3, // затримка на основі індексу
       },
     },
@@ -50,17 +59,14 @@ const CompletedProjectCard = ({
       ref={ref}
       variants={cardVariants}
       initial="hidden"
-      animate={isInView ? "visible" : "hidden"} // тригер анімації
+      animate={inView ? "visible" : "hidden"}
       className="inline-block rounded-lg shadow-md bg-white"
     >
       <CardContent className="p-10 flex flex-col justify-center items-center gap-4">
-        {/* Number */}
         <h2 className="text-4xl font-bold text-blue-900">
           {value}
           {additionalTitle}
         </h2>
-
-        {/* Icon and Text */}
         <div className="flex items-center space-x-2 mt-2">
           {icon}
           <p className="text-md font-medium text-blue-900">{description}</p>
