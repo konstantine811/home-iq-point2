da9d13700872e5b22674c58200566dc8c3e44c80
konstantine811 <galystyan777@gmail.com>
2024-10-23 13:30:56 +0300

    Refactor RedButton component: Remove framer-motion dependency

--- src/components/pages/home/red-button.tsx
+++ src/components/pages/home/red-button.tsx
@@ -1,7 +1,4 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
-import { motion } from "framer-motion";
 
 interface Props {
   title: string;
@@ -10,13 +7,13 @@ interface Props {
 
 const RedButton = ({ title, className = "" }: Props) => {
   return (
-    <motion.div whileTap={{ scale: 0.9 }}>
+    <div>
       <Button
         className={`min-w-40 w-full bg-red-700 hover:bg-red-700 transition-all hover:scale-105 focus:outline-none focus:ring-2 focus:ring-red-700 focus:ring-opacity-50 font-extrabold rounded-xl h-12 ${className}`}
       >
         {title}
       </Button>
-    </motion.div>
+    </div>
   );
 };
 
